f3aee4c77daa3893cdd3ee15894a970cb36dd466
Kyle Pereira <kpereira0521@sdsu.edu>
2022-04-24 22:05:44 -0700

    NetworkController script added

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -1,13 +1,18 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworkController : MonoBehaviour
+public class NetworkController : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster(){
+        Debug.Log("We are now connexted to: " + PhotonNetwork.CloudRegion);
     }
 
     // Update is called once per frame
