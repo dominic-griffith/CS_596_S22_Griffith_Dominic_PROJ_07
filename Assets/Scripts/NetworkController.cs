ef7f776b0c8a3cc3c9783b82cb33a53f4ca867ac
Kyle Pereira <kpereira0521@sdsu.edu>
2022-04-24 22:33:38 -0700

    fixes to NetworkController script

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,16 @@ public class NetworkController : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Photon Network
     }
 
     public override void OnConnectedToMaster(){
-        Debug.Log("We are now connexted to: " + PhotonNetwork.CloudRegion);
+        Debug.Log("We are now connected to: " + PhotonNetwork.CloudRegion); 
+        RoomOptions roomOptions = new RoomOptions(); //creates options for multiplayer room
+        roomOptions.IsVisible = false;
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom("Salinity", roomOptions, TypedLobby.Default); //checks to see if room exists with name and joins or creates
+
     }
 
     // Update is called once per frame
