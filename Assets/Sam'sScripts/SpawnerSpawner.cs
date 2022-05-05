a909237d8b936837327e11af383aecef69647907
Samuel Schneider <sschneider2883.@sdsu.edu>
2022-05-05 12:20:05 -0700

    added music

---
+++ Assets/Sam'sScripts/SpawnerSpawner.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerSpawner : MonoBehaviour
+{
+    public BoidSupervisor spawnerPrefab;
+    public List<Boid>
+}
